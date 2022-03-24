f7721e68b5fb04613ff760d018f0030a16ad2ae0
gopalbhandari <gopalbhandari9991.com>
2022-03-24 16:07:49 +0530

    updated

---
+++ Typescript/Day16 activity/export.ts
@@ -0,0 +1,16 @@
+export class Employee {
+    id: number;
+    firstName: string;
+    lastName: string;
+
+    constructor(id: number, firstName: string, lastName: string) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    getFullName() {
+        return this.id+' '+ this.firstName + ' ' + this.lastName;
+    }
+}
+
